de348d18527df324b7ea7fb746d35049f2747f6b
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-07-18 19:56:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,295 @@
+// let name = "Abdullox";
+// let surname = 'Qodirov';
+// let num1 = `salom`;
+// console.log(typeof num1);
+
+
+// let a = 4;
+// let b = 6;
+// // let c= a!=b;
+// let c = a!==b
+
+
+// console.log(c);
+
+// let num = +prompt("son kiritin");
+
+// let res = "";
+// if (num > 0) {
+//     res = "musbat"    
+// }else{
+//     res = "musbat emas"
+// }
+// console.log(res);
+
+// let a = +prompt("son kiritin");
+// let b;
+// if (a>0) {
+//     b=++a
+// }else{
+//     b=a-2
+// }
+// console.log(b);
+
+// // 1-misol
+// let a = -23;
+// let res ;
+// if (a>0) {
+//     ++a
+// };
+// console.log(a);
+
+// 2-misol
+// let a = -23;
+// let res ;
+// if (a>0) {
+//     ++a
+// }else{
+//     a-=2
+// };
+// console.log(a);1
+
+// 3-misol 
+// let a = -10;
+// let res ;
+// if (a>0) {
+//     ++a
+// }else if (a==0) {
+//     a+=10
+// } else {
+//     a-=2
+// };
+// console.log(a);
+
+// 4-misol
+// let a = 2;
+// let b = 3;
+// let c = 4;
+// counter = 0;
+// if (a>0) {
+//     counter +=1
+// }if (a>0) {
+//     counter +=1
+// }if (a>0) {
+//     counter +=1
+// };
+// console.log(counter);
+
+// 5-misol
+// let a = 2;
+// let b = 3;
+// let c = 4;
+// let musbat = 0;  
+// let manfiy = 0;
+// if (a> 0) {
+//     musbat++;
+// }else{
+//     manfiy++
+// }if (b> 0) {
+//     musbat++;
+// }else{
+//     manfiy++
+// }if (c> 0) {
+//     musbat++;
+// }else{
+//     manfiy++
+// }
+// console.log(manfiy+ " manfiy");
+// console.log(musbat+  ' musbat');
+
+// 6-misol
+// let a = 5;
+// let b = 3;
+// if (a>b) {
+//     console.log(a);
+// }else{
+//     console.log(b);
+// };
+
+// 7-misol
+// let a = 5;
+// let b = 3;
+// if (a<b) {
+//     console.log(a);
+// }else{
+//     console.log(b);
+// };
+
+// 8-misol
+// let a = 2;
+// let b =4;
+// let max;
+// let min;
+// if (a>b) {
+//     max = a;
+//     min = b;
+// }else{
+//     max = b;
+//     min = a;
+// }
+// console.log(max);
+// console.log(min);
+
+
+// 9-masala
+// let A = +prompt("A  ");
+// let B = +prompt("B  ");
+// let c;
+// if (A>B) {
+//     c = B;
+//     B = A;
+//     A = c;
+// }
+
+// 10-masala
+// let A = +prompt("A");
+// let B = +prompt("B");
+// let max;
+// if (A == B) {
+//   A = 0;
+//   B = 0;
+// } else if (A > B) {
+//   max = A;
+//   A = A;
+//   B = A;
+// } else if (B > A) {
+//   max = B;
+//   B = B;
+//   A = B;
+// }
+
+// 11- masala
+// let A = +prompt("A");
+// let B = +prompt("B");
+
+// if (A == B) {
+//     A = 0;
+//     B = 0;
+// } else if (A > B) {
+//     A = A;
+//     B =A;
+// } else if (B > A) {
+//     A = B;
+//     B = B;
+// }
+
+// 12- masala
+// let a = +prompt(");
+// let b = +prompt("b");
+// let c = +prompt("c");
+// let min;
+// if (a > b) {
+//     min = a;
+// } else {
+//     min = b
+// }
+// if (min > c) {
+//     min = c;
+// }
+// console.log(min);
+ 
+// 13-misol
+// let a = 2;
+// let b = 3;
+// let c = 5;
+// let res;
+// if (a>=b && a<=c) {
+//     res = a;   
+// }
+// if (b>=a && b<=c) {
+//     res = b;   
+// }
+// if (c>=a && c<=b) {
+//     res = c;   
+// }
+// console.log(res);
+
+// 14-misol 
+// let a = 2;
+// let b = 3;
+// let c = 4;
+// let max ;
+// let min;
+// if (a>b) {
+//     max=a;
+//     min=b;
+// }else{
+//     max=b;
+//     min=a
+// }
+// if (max<c) {
+//     max = c;
+// }
+// console.log(min);
+// console.log(max);
+
+// // 15-miso
+// let a = 2;
+// let b = 3;
+// let c = 4;
+// let max ;
+// let d;
+// if (a>b) {
+//     max=a;
+// }else{
+//     max=b;
+// }
+// if (max<c) {
+//     max = c;
+// }
+// if (a>=b && a<=c || a<=b && a>=c) {
+//     d=a
+// }
+// if (b>=a && b<=c || b<=a && b>=c) {
+//     d=b
+// }
+// if (c>=a && c<=b || c<=a && c>=b) {
+//     d=c
+// }
+// console.log(max);
+// console.log(d);
+  
+// BOOLIN
+//  1-misol
+// let a = 12;
+// let res ;
+// if (a>0) {
+//     res = true;
+// }else{
+//     res = false;
+// }
+// console.log(res);
+
+// 2-misol
+// let a = 13;
+// let res;
+// if (a % 2 ==1) {
+//     res = true
+// }else{
+//     res = false;
+// }
+// console.log(res);
+
+// 3-misol
+// let a = 13;
+// let res;
+// if (a % 2 ==0) {
+//     res = true
+// }else{
+//     res = false;
+// }
+// console.log(res);
+
+// // 4-misol
+// let a = 23;
+// let b = 12;
+// let res;
+// if (a>2 &&b<=3) {
+//     res= true;
+// }else{
+//     res = false;
+
+// }
+// console.log(res);
+
+// 5-misol
